9ed5c40002002deb4d6c70c37879dc64404162f5
Maria <bambass36@gmail.com>
2018-04-06 07:46:32 +0200

    exercise opentraveldata without csvkit updated

--- shell/ex2_nocsvkit.sh
+++ shell/ex2_nocsvkit.sh
@@ -6,8 +6,10 @@ echo "Hoy es 'date'"
 # Go to ~/Data/opentraveldata
 
 echo "1. Use grep to extract al 7x7 or 3xx (where x can be any number) airplane models from optd_aircraft.csv"
+
 cat ~/Data/opentraveldata/optd_aircraft.csv | cut -d '^' -f3 | grep '^3' | uniq -c
-cat ~/Data/opentraveldata/optd_aircraft.csv | cut -d '^' -f3 | grep '^7' | grep -E '[%7]'
+cat ~/Data/opentraveldata/optd_aircraft.csv | cut -d '^' -f3 | grep '^7.7' | uniq -c
+
 
 echo "2. Use grep to obtain the number of airlines with prefix 'areo'in their name from optd_airlines.csv"
 cat ~/Data/opentraveldata/optd_airlines.csv | cut -d '^' -f8,9 | grep '^[a|A]ero' |sort |uniq -c |wc -l
