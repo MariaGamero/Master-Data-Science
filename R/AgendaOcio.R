407d7549c64ddd4a4183330844898913859577fc
María <37455195+MariaGamero@users.noreply.github.com>
2018-05-29 19:49:44 +0200

    Adding plot and description

--- R/AgendaOcio.R
+++ R/AgendaOcio.R
@@ -1,27 +1,51 @@
-#####################################################################
-#Install Libraries
-#####################################################################
+####################################################################################
+#Exercise: Conocer la distribucion en horas de los cursos realizados en Madrid
+#y por Distritos 
+####################################################################################
 
-list.of.packages <- c("R.utils", "tidyverse", "doParallel", "foreach", "sqldf", "broom", "DBI", "ggplot2", "tidyr", "lubridate")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+#Install Libraries..................................................................
 
 library("dplyr")
 library("data.table")
 library("tidyr")
 
+#Cargamos el fichero................................................................
 
-agenda <- fread("dataR/agendaMadrid/2007.csv")
-class(agenda)
+agenda <- fread("DataR/agendaMadrid/2007.csv")
 
-agenda %>% 
+#Hacemos la consulta y al resultado lo denominamos AgendaOcioMadrid.................
+agendaOcioMadrid<- agenda %>% 
   mutate(FECHA= as.Date(FECHA)) %>% 
   mutate(`FECHA-FIN` = as.Date(`FECHA-FIN`)) %>% 
-  mutate(duracionDias=(`FECHA-FIN`-FECHA))%>% 
-  separate(FECHA, c("COMIENZO","day","hour"),c(7,9)) %>%
-  filter(duracionDias>0,!is.na(DISTRITO)) %>% 
-  select(COMIENZO,`TITULO-ACTIVIDAD`,`DIAS-SEMANA`,DISTRITO,duracionDias,GRATUITO) %>% 
-  group_by(DISTRITO) %>%
-  arrange(-duracionDias)
+  mutate(Duracion=(`FECHA-FIN`-FECHA))%>% 
+  filter(Duracion>0, DISTRITO !="") %>% 
+  group_by(DISTRITO,Duracion) %>% 
+  summarise(TotalCursos=n(),HorasImpartidas=as.integer(sum(Duracion)))%>% 
+  arrange(-HorasImpartidas)
+
+#quitar outliers....................................................................
+outliers<- boxplot.stats(agendaOcioMadrid$HorasImpartidas)$out
+length(outliers)
+outliers
+
+#Creamos una nueva variable:agendaOcioMadrid_SinOutliers
+agendaOcioMad_SinOutliers <- agendaOcioMadrid %>% 
+  filter(!HorasImpartidas %in% outliers) 
+
+#borramos el data anterior 
+rm(agendaOcioMadrid)
+
+#Graficamente.........................................................................
+library(ggmap)
+library(tidyverse)
+library(ggplot2)
+
+spread(agendaOcioMad_SinOutliers)
+table(agendaOcioMad_SinOutliers$HorasImpartidas)
+
+gr.horas=cut(agendaOcioMad_SinOutliers$HorasImpartidas,breaks=seq(0,60,10))
 
 
+qplot(TotalCursos,gr.horas,data=agendaOcioMad_SinOutliers,color=DISTRITO,geom=c("point","smooth"),
+      xlab="Número de Cursos")
+  
