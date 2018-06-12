420f277fec4a3271cd5125086054d53e9eb40068
María <37455195+MariaGamero@users.noreply.github.com>
2018-06-12 21:18:04 +0200

    adding visualitation. almost finished

--- R/MundialFutbol.R
+++ R/MundialFutbol.R
@@ -24,7 +24,7 @@ names(mundialesGanados)<-c("Equipo","ID","Ganados","EdicionesGanadas")
 
 #tabla3.- Tarjetas
 tarjetas <- mundiales %>% html_nodes("table") %>% .[[5]] %>% html_table(header=TRUE)
-names(tarjetas)<-c("Equipo","ID" ,"NumeroTarjetas" ,"TA", "2TA","TR","Ediciones","PartidosJugados" )
+names(tarjetas)<-c("Equipo","ID" ,"NumeroTarjetas" ,"1TA", "2TA","TR","Ediciones","PartidosJugados" )
 
 
 ######################################################################
@@ -47,7 +47,7 @@ summary(tarjetas)
 ######################################################################
 
 mundial<-merge(x = mundialesGanados[,c("ID","Ganados")], y =clasificacion , by = "ID", all.y = TRUE)
-mundial<-merge(x = tarjetas[,c("ID","NumeroTarjetas","TA","2TA","TR","Ediciones")], y =mundial , by = "ID", all.y = TRUE)
+mundial<-merge(x = tarjetas[,c("ID","NumeroTarjetas","1TA","2TA","TR","Ediciones")], y =mundial , by = "ID", all.y = TRUE)
 head(mundial)
 
 rm(clasificacion)
@@ -76,7 +76,7 @@ mundial$Ediciones=as.factor(mundial$Ediciones)
 datosMundial<-mundial %>% 
   filter(!is.na(Ediciones)) %>% 
   mutate(Ganados=replace_na(Ganados, "0")) %>% 
-  select(Equipo,Partic.,Ganados,Pts.,PJ,PG,PE,PP,GolesMarcados,GolesContra,PromedioPuntos.,NumeroTarjetas,TA,"2TA",TR,Ganados) %>% 
+  select(Equipo,Partic.,Ganados,Pts.,PJ,PG,PE,PP,GolesMarcados,GolesContra,PromedioPuntos.,NumeroTarjetas,"1TA,"2TA",TR,Ganados) %>% 
   arrange(desc(Pts.))
 
 head(datosMundial)
@@ -84,14 +84,39 @@ head(datosMundial)
 ######################################################################
 #Visualitation
 ######################################################################
+
 #gráfico 1
+tarjetas <- datosMundial %>% 
+  select(Equipo,"1TA","2TA",TR)%>% 
+  gather(clase,NumeroTarjetas,-Equipo) %>%  
+  group_by(Equipo) %>% 
+  arrange(Equipo) %>%
+  ggplot(aes(x=clase,y=NumeroTarjetas,color=clase),size=3,alpha=.2,label=NumeroTarjetas)+
+  geom_point(stat="identity")+facet_wrap(~Equipo)+labs(x="Tipo de Tarjeta")+
+  labs(title="Detalle del total tarjetas  por país",
+       subtitle="Mundiales 1973-2004", 
+       x="Tipo de Tarjeta")+ 
+  scale_color_manual("clase",values=c("yellow1","orange1","red2"))+
+  geom_text(aes(label=NumeroTarjetas),col="black",hjust=0, vjust=-1.5,size=3)+
+  theme(axis.text.x = element_text(angle = 90,    #Roto 90° las etiquetas del eje x
+                                   size = 6,      # Reduzco el tamaño a 6 puntos 
+                                   color="blue"))+ #Cambio el color del texto a azul 
+  theme(axis.text.y = element_text(size = 6,      
+                                   color="blue")) 
+
+
+
+#gráfico 2
 datosMundial %>% 
   filter(Ganados!= 0) %>% 
   ggplot(aes(x=Equipo,y=Partic.,color=Equipo,size=Ganados))+geom_point()+
   labs(x="Equipo", y="Participaciones")+ ggtitle("Detalle de mundiales por país")
+  geom_text(aes(label=Ganados),col="black",hjust=2.5, vjust=.5,size=4)
+
+#ggsave("ParticipaciónyMundialesGanadosPorPais.pdf", width = 18, height = 15, units = "cm")
 
 
-#gráfico 2
+#gráfico 3
 ggplot(data=datosMundial,aes(x=Pts.,y=reorder(Equipo,Pts.),color=Pts.))+
   geom_point()+labs(x="Total puntos", y="Equipo")+
   ggtitle("Clasificación total puntos obtenidos en los mundiales 1973-2004")
